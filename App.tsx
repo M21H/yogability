0d6b5875647c5c0fedb4ec2890b5507315740ec2
M21H <maxym.hyba@gmail.com>
2023-06-19 18:37:31 +0300

    add splash screen to android

--- App.tsx
+++ App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Yogability} from './src';
+import SplashScreen from 'react-native-splash-screen';
 
 const APP_URI = 'https://yogabills.herokuapp.com/';
 const APP_ACCENT_COLOR = '#5D53A4';
 
 function App(): JSX.Element {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <SafeAreaProvider style={{backgroundColor: APP_ACCENT_COLOR}}>
       <StatusBar barStyle="light-content" />
